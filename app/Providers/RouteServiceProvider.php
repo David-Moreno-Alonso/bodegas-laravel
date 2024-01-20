deb164f610638d7ba7136de3dab52c86c966575a
David-Moreno-Alonso <david.moreno@ikasle.egibide.org>
2024-01-20 14:06:31 +0100

    inicio de sesion y rutas seguras

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/bodegas';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
